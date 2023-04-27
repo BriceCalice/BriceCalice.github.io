66d0e38044926a1b21a24d989a24444755ca9aa4
Jean-Calice <admin@quickerbay.com>
2023-04-27 22:09:15 +0400

    Food section added

--- message/src/index.js
+++ message/src/index.js
@@ -1,1 +1,15 @@
-console.log('hello world');
+const bannerMessage = document.getElementById('welcome-banner');
+const hour = new Date().getHours();
+
+const header2 = message => {
+    return `<h2>${message}</h2>`;
+}
+
+// console.log(header2("hello"));
+if (hour < 12) {
+    bannerMessage.innerHTML = header2('Good Morning <span class="banner-span"><i>My Love</i> <img src="src/images/red_ribbon.png" /></span>');
+} else if ( hour < 17){
+    bannerMessage.innerHTML = header2('Good Afternoon <span class="banner-span"><i>My Love</i> <img src="src/images/red_ribbon.png" /></span>');
+} else {
+    bannerMessage.innerHTML = header2('Good Evening <span class="banner-span"><i>My Love</i> <img src="src/images/red_ribbon.png" /></span> ');
+};
